e9e3fe496d5b0303aa13b67c0cb6309f953280b8
tristan bellanger <tristanbellanger10@gmail.com>
2024-11-17 16:39:53 +0900

    feat: implement user authentication and logout functionality
    - Added Devise for user authentication
    
    - Configured routes and viewa for sign-up, login, and logout
    
     Implemented username and wallet adress fields in the user model
    
    - Customized Devise sign-up form to include new fields
    
    - Added navigation links for sign-up, login, and logout
    
    - Resolved logout routing issue using button_to with data: { turbo: false }
    
    - Set up a home#index as the root route
    
    - Ensured proper Javascript setup for UJS compatibility

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,8 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+//= require jquery
+//= require jquery_ujs
+
 import "@hotwired/turbo-rails"
 import "controllers"
+import "@rails/ujs";
+Rails.start(); // Ensure Rails UJS is started
